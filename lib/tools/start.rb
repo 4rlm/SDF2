fc4eae858f3965c986b8716586daef68c11581c9
Adam Booth <4rlm@protonmail.ch>
2018-01-09 17:48:52 -0600

    preparing to re-seed all data, then run all processes up till and including ActScraper, then start on ContScraper.

--- lib/tools/start.rb
+++ lib/tools/start.rb
@@ -3,56 +3,71 @@
 #Call: Start.method_name
 class Start
 
-  ###################
-  ## == IMPORTS-EXPORTS == ##
-  ###################
+  ##############################
+  ###### IMPORTS-EXPORTS #######
+  ##############################
 
-  #CALL: Start.import_seeds
+  # 1) CALL: Start.import_seeds
   def self.import_seeds
     CsvTool.new.import_all_seed_files
   end
 
 
-  #CALL: Start.create_backups
+  # 2) CALL: Start.create_backups
   def self.create_backups
     CsvTool.new.backup_entire_db
   end
 
 
-  #CALL: Start.restore_backups
+  # 3) CALL: Start.restore_backups
   def self.restore_backups
     CsvTool.new.restore_all_backups
   end
 
 
-  ###################
-  ## == VERIFIERS == ##
-  ###################
+  ##############################
+  ######### VERIFIERS ##########
+  ##############################
 
 
-  #CALL: Start.verify_urls
+  # 4) CALL: Start.verify_urls
   def self.verify_urls
     UrlVerifier.new.start_url_verifier
   end
 
 
-  #CALL: Start.find_templates
+  ##############################
+  ########## FINDERS ###########
+  ##############################
+
+
+  # 5) CALL: Start.find_templates
   def self.find_templates
     TemplateFinder.new.start_template_finder
   end
 
 
-  #CALL: Start.find_pages
+  # 6) CALL: Start.find_pages
   def self.find_pages
     PageFinder.new.start_page_finder
   end
 
 
-  #CALL: Start.scrape_acts
+  ##############################
+  ########## SCRAPERS ##########
+  ##############################
+
+
+  # 7) CALL: Start.scrape_acts
   def self.scrape_acts
     ActScraper.new.start_act_scraper
   end
 
+
+  # 8) CALL: Start.scrape_conts
+  # def self.scrape_conts
+  #   ContScraper.new.start_cont_scraper
+  # end
 
 
 
