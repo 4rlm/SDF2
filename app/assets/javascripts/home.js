2f464127889602a00c598d0982859392a017d11c
Adam Booth <4rlm@protonmail.ch>
2018-04-12 06:34:08 -0500

    Created parallax homepage in root.  Looks nice, but needs to be customized.  will add and change content next.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,19 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(window).scroll(function() {
+  parallax();
+})
+
+function parallax() {
+
+  var wScroll = $(window).scrollTop()
+
+
+  $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+
+
+  $('.parallax--box').css('top', -5 + (wScroll*.005)+'em')
+
+}
